8f05deaa55e1a87e5437186b6c9e010ec8d9d505
yohannesjoya <yohannesdestagebru10@gmail.com>
2022-11-06 21:55:00 +0300

    Added Day 15 CoffeeMachine Project,
    this is the first commit for this project

---
+++ Day 15 - Coffee Machine/coffe.py
@@ -0,0 +1,83 @@
+
+
+# # services
+espresso={'water':50, 'coffee':18,'milk':0,'price':1.5}
+latte = {'water':200,'coffee':24, 'milk':150,'price':2.5}
+cappuccino={'water':250,'coffee':24, 'milk':100,'price':3}
+
+# #resources
+resources={"waterT" : 300 ,"coffeeT" : 100 , "milkT" : 200 , 'Money':0}
+
+# # coins
+coins={"penny" :0.01,"nickel": 0.05,"quarter": 0.25,"dime":0.1 }
+
+mychoice = vars()
+
+def seym(choice):
+    if choice=='latte':
+        mychoice[choice]= latte
+    elif choice=='espresso':
+        mychoice[choice]=espresso
+    elif choice=="cappuccino":
+        mychoice[choice]=cappuccino
+    else:
+        print("Wrong choice")
+    return mychoice[choice]
+    
+
+
+def calculateServe(choice):
+    FinalChoice=seym(choice)
+   
+    
+    if resources['waterT']<FinalChoice['water']:
+        print(f"there is no enough Water for {choice}")
+        # do this
+    else:
+       if resources['coffeeT']<FinalChoice['coffee']:
+           print(f"there is no enough coffee for {choice}")
+       else:
+           if resources['milkT']<FinalChoice['milk']:
+               print(f"there is no enough milk for {choice}")
+           else:
+               print('Please insert Coins.')
+               paymentQuarters = float(input("How many Quarter?: ")) 
+               paymentDimes = float(input("How many Dime?: ")) 
+               paymentNickels = float(input("How many Nickels?: "))
+               paymentPennies = float(input("How many Pennies?: "))
+               TotalPayment = (coins['penny']*paymentPennies)+(coins['quarter']*paymentQuarters)+(coins['dime']*paymentDimes)+(coins['nickel']*paymentNickels)
+               if TotalPayment>= FinalChoice['price']:
+                   change = TotalPayment-FinalChoice['price']
+                   resources['Money']+=FinalChoice['price']
+                   
+                   resources['waterT']-=FinalChoice['water']
+                   resources['milkT']-= FinalChoice['milk']
+                   resources['coffeeT']-=FinalChoice['coffee']
+                   
+                   print(f'Here is {change} in change.')
+                   print(f'Here is your {choice} Enjoy')
+
+                   
+               else:
+                   print(f"you dont have enough money to buy this item :your money={TotalPayment}: and the price={FinalChoice['price']}")
+
+
+def coffee():
+    choice = input("what would you like? (espresso/latte/cappuccino): ")
+    if choice=='report':
+        print(f"Water: {resources['waterT']}ml")
+        print(f"Milk: {resources['milkT']}ml")
+        print(f"Coffee: {resources['coffeeT']}g")
+        print(f"Money: ${resources['Money']}")
+        coffee()
+        
+    else:
+        calculateServe(choice)
+        coffee()
+
+    
+    
+coffee()            
+
+
+
